5571d70894183eb6a7ddc1fca4a7f61bc0dc06e0
juanmanuelrdn <juanmanuelrdn@gmail.com>
2019-05-13 16:47:41 +0200

    - Abonado: Editado atributos Añadido getters y setters Añadido constructor por defecto

--- src/funcionalidad/Abonado.java
+++ src/funcionalidad/Abonado.java
@@ -10,14 +10,64 @@ package funcionalidad;
  * @author Usuario
  */
 public class Abonado {
-    String dni;
-    String nombre;
-    String apellidos;
-    int numTarjeta;
-    enum tipoAbono {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int numTarjeta;
+    private enum tipoAbono {
         MENSUAL,
         TRIMESTRAL,
         SEMETRAL,
         ANUAL
     }
+    private String email;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getNumTarjeta() {
+        return numTarjeta;
+    }
+
+    public void setNumTarjeta(int numTarjeta) {
+        this.numTarjeta = numTarjeta;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Abonado(){
+        dni = "1234567";
+        nombre = "nombredefault";
+        apellidos = "apellidosdefault";
+        numTarjeta = 1111111;
+        email = "email@default.com";
+    }
+    
 }
