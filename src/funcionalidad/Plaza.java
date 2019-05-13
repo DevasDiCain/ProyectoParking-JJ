b2ad8fcbd7fb16d5e15ca985b25071366ad5c041
juanmanuelrdn <juanmanuelrdn@gmail.com>
2019-05-13 16:50:14 +0200

    - Plaza: Creada Añadido atributos

---
+++ src/funcionalidad/Plaza.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funcionalidad;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Plaza {
+    boolean ocupado;
+    boolean reservado;
+    private enum tipoPlaza {
+        TURISMO,
+        MOTOCICLETA,
+        CARAVANA
+    }
+}
