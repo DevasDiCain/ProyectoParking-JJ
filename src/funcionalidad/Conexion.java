9fc79be118e1129c37e177e227b5731fe0fe9611
juanmanuelrdn <juanmanuelrdn@gmail.com>
2019-05-13 16:28:18 +0200

    - Creación del paquete: Funcionalidad
    - Creación de la clase:
    Conexion

---
+++ src/funcionalidad/Conexion.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funcionalidad;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Conexion {
+
+    private static final String SERVIDOR = "jdbc:mysql://192.168.1.127/";
+    private static final String NOMBRE_BASE_DATOS = "prueba";
+    private static final String USER = "root";
+    private static final String PASS = "123456";
+
+    private static Connection instancia = null;
+    
+    // Constructor privado no accesible desde otras clases
+    private Conexion() {
+
+    }
+
+    // Método de clase para acceder a la instancia del objeto Connection
+    public static Connection getInstance() {
+        // Si el objeto Connection no está creado, se crea
+        if (instancia == null) {
+            try {
+
+                // Se crea el objeto Connection	
+                instancia = DriverManager.getConnection(SERVIDOR + NOMBRE_BASE_DATOS, USER, PASS);
+
+                System.out.println("Conexión realizada con éxito.");
+
+            } catch (SQLException e) {
+
+                // Error en la conexión
+                System.out.println("Conexión fallida: " + e.getMessage());
+            }
+        }
+        return instancia;
+    }
+}
