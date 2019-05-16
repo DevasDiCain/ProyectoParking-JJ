495a92d8fa7c02fe270aec14a5b1cf7f71d8da70
DevasDiCain <peperodriguezfdz@gmail.com>
2019-05-16 17:18:40 +0200

    -Creación del Menu Control Plazas

--- src/vista/ZonaAdministrador.java
+++ src/vista/ZonaAdministrador.java
@@ -237,7 +237,9 @@ public class ZonaAdministrador extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        // ControlPlazas:
+        new MenuControlPlazas().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
