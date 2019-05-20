5fb042115e5fbd32b8860c56bff56177f4faf44a
JuanManuelRDN <juanmanuelrdn@gmail.com>
2019-05-20 11:52:12 +0200

    - Se ha terminado la interfaz del ticket

--- src/modelo/ITicket.java
+++ src/modelo/ITicket.java
@@ -5,10 +5,33 @@
  */
 package modelo;
 
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
- * @author juan
+ * @author jose
  */
 public interface ITicket {
+     // Método para obtener todos los registros de la tabla
+    List<TicketVO> getAll() throws SQLException;
     
+    // Méodo para obtener un registro a partir de la PK
+    TicketVO findByPk(int codticket) throws SQLException;
+    
+    // Método para insertar un registro
+    int insertTicket (TicketVO ticket) throws SQLException;
+    
+    // Método para insertar varios registros
+    int insertTicket (List<TicketVO> lista) throws SQLException;
+    
+    // Método para borrar una persona
+    int deletePersona (TicketVO ticket) throws SQLException;
+    
+    // Método para borrar toda la tabla
+    int deletePersona() throws SQLException;
+    
+    // Método para modificar un ticket. Se modifica al ticket que tenga esa 'pk'
+    // con los nuevos datos que traiga el ticket 'nuevosDatos'
+    int updatePersona (int codticket, TicketVO nuevosDatos) throws SQLException;
 }
