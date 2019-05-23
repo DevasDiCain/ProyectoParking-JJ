a2f54c3ad29804e30ce1c5e3e96e97605bb98cd1
Devas <peperodriguezfdz@hotmail.com>
2019-05-23 14:43:24 +0200

    -Modificaciones en la creación de ficheros

--- src/copiaSeguridad/crearFicheros.java
+++ src/copiaSeguridad/crearFicheros.java
@@ -6,6 +6,8 @@
 package copiaSeguridad;
 
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,6 +15,9 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.List;
+import modelo.AbonadoVO;
+import modelo.EnviarDatos;
 
 /**
  *
@@ -34,22 +39,39 @@ public class crearFicheros {
        crearFicheros.crearCopiaSeguridad("Abonado");
     }
     
-    public static void crearCopiaSeguridad(String carpeta){
-        String subCarpeta=crearFicheros.crearSubCarpeta();
-        Path origen  = Paths.get("./Aparka_"+carpeta+".sql");
-        Path destino = Paths.get("./"+subCarpeta+"copia"+carpeta+".txt");
-        try
-        {  
-            Files.copy(origen, destino);
-        }
-        catch(IOException e) {     
-            System.out.println("Problema copiando el archivo.");
-            System.out.println(e.toString());
-        }
+    public static void crearCopiaSeguridadAbonado(List<AbonadoVO>  lista){
+       // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
+        String idfichero = "Abonado.txt";
+        
+        // Array a escribir
+         lista =EnviarDatos.obtenerAbonados();
+
+       // Estructura try-with-resources. Instancia el objeto con el fichero a escribir
+       // y se encarga de cerrar el recurso "flujo" una vez finalizadas las operaciones
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idfichero))){
+            
+            
+            for (int i = 0 ; i < lista.size(); i++) {
+                for (int j = 0; j < lista.size(); j++) {
+                    // Usamos metodo write() para escribir en el buffer
+                    flujo.write(i + ",");
+                }
+                // Metodo newLine() añade línea en blanco
+                flujo.newLine();
+            }
+            // Metodo flush() guarda cambios en disco 
+            flujo.flush();
+            
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } 
+
+    }
 
         
-    }
+    
     public static void restaurarCopiaSeguridad(){}
     
 }
+
 
