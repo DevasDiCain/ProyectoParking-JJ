6f285cb92d60d5ce4cc61f9ade56e056f0aa5c2e
Devas <peperodriguezfdz@hotmail.com>
2019-05-23 14:13:07 +0200

    -Método que creará las subcarpetas de la copia de seguridad

--- src/copiaSeguridad/crearFicheros.java
+++ src/copiaSeguridad/crearFicheros.java
@@ -5,10 +5,31 @@
  */
 package copiaSeguridad;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
 /**
  *
  * @author jose
  */
 public class crearFicheros {
-    
+
+    public static void crearSubCarpeta(LocalDate dia, LocalTime hora) {
+        Path directory = Paths.get(String.valueOf(dia)+"---"+String.valueOf(hora)+"./files");
+        try {
+            Files.createDirectory(directory);
+        } catch (IOException e) {
+            System.out.println("Problema creando el directorio.");
+            System.out.println(e.toString());
+        }
+    }
+    public static void main(String[] args) {
+        crearFicheros.crearSubCarpeta(LocalDate.of(1998, Month.MARCH, 1), LocalTime.of(15, 0));
+    }
 }
