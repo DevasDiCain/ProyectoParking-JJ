88d673e8b3f47c9c9cb52c9035875b44f676f757
JuanManuelRDN <juanmanuelrdn@gmail.com>
2019-06-06 10:40:38 +0200

    -CalcularPrecio Optimización del método

--- src/funcionalidad/CalcularPrecio.java
+++ src/funcionalidad/CalcularPrecio.java
@@ -8,6 +8,7 @@ package funcionalidad;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import javax.swing.JOptionPane;
 import modelo.EnviarDatos;
@@ -20,21 +21,26 @@ import modelo.TicketVO;
  */
 public class CalcularPrecio {
     public static double calcularPrecioTicket(String matricula, String identificador, String pin) {
+        int contador = 0;
         double precio = 0;
         int minutosTotalesAparcado = 0;
-        int contador = 1;
+        int minutosEntreDias = 0;
+        
         List<TicketVO> listaDeTickets = EnviarDatos.obtenerTickets();
-       
         for (TicketVO ticket : listaDeTickets) {
             if (ticket.getMatricula().equalsIgnoreCase(matricula) && ticket.getImporte() == 0) {
-                int diasEntreDepositoYRetiro = (int) Duration.between(ticket.getFechaEntrada().atStartOfDay(), LocalDate.now().atStartOfDay()).toDays();
+                long diasEntreEntradaSalida = ChronoUnit.DAYS.between(ticket.getFechaEntrada(), LocalDate.now());
                 int minutosTotalesHoy = (LocalTime.now().getHour() * 60) + LocalTime.now().getMinute();
                 int minutosTotalesTicket = (ticket.getHoraEntrada().getHour() * 60) + ticket.getHoraEntrada().getMinute();
-                if (diasEntreDepositoYRetiro == 0) {
+                if (diasEntreEntradaSalida < 1) {
                     minutosTotalesAparcado = minutosTotalesHoy - minutosTotalesTicket;
                 } else {
-                    minutosTotalesAparcado = 1440 * diasEntreDepositoYRetiro - minutosTotalesTicket + minutosTotalesHoy;
-                } 
+                    if (diasEntreEntradaSalida > 1) {
+                        diasEntreEntradaSalida -= 1;
+                        minutosEntreDias = (int) (diasEntreEntradaSalida * 1440);
+                    }
+                    minutosTotalesAparcado = minutosEntreDias + minutosTotalesHoy + (1440 - minutosTotalesTicket);
+                }
                 PlazaVO plaza = EnviarDatos.obtenerPlazaSegunPk(ticket.getCodPlaza());
                 switch (plaza.getTipoPlaza()) {
                     case "turismo":
@@ -50,7 +56,7 @@ public class CalcularPrecio {
                         break;
                 }
                 ticket.setHoraSalida(LocalTime.now());
-                ticket.setImporte((int) precio);
+                ticket.setImporte(precio);
                 EnviarDatos.cambiarTicket(ticket.getCodTicket(), ticket);
             } else {
                 System.out.println("El ticket " + contador + " no era el correcto");
