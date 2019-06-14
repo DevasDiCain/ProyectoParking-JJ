b5ffd6385c7a4e5cce672196d76e0ed1ccc9c961
juanmanuelrdn <juanmanuelrdn@gmail.com>
2019-06-15 01:54:28 +0200

    -restaurarCopiaDeSeguridad Al devolver los contenidos de un fichero, devolverá null si el interior está vacío Además, también se ha arreglado el array que se utiliza para poder introducir los datos dentro de la base de datos*
    *falta por implementar en cada clase

--- src/copiaSeguridad/restaurarCopiaDeSeguridad.java
+++ src/copiaSeguridad/restaurarCopiaDeSeguridad.java
@@ -5,6 +5,7 @@
  */
 package copiaSeguridad;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -29,12 +30,18 @@ import modelo.VehiculoVO;
  */
 public class restaurarCopiaDeSeguridad {
 
-    public static ArrayList<AbonadoVO> restaurarTablaAbonados() {
+    public static ArrayList<AbonadoVO> restaurarTablaAbonados() throws FileNotFoundException, IOException {
+
         ArrayList<AbonadoVO> abonados = new ArrayList<>();
         AbonadoVO abonado = new AbonadoVO("");
-        
+
         // Fichero a leer
-        String idFichero = "./backup/"+restaurarCopiaDeSeguridad.hallarUltimaCarpeta()+"/Abonado.txt";
+        String idFichero = "./backup/" + restaurarCopiaDeSeguridad.hallarUltimaCarpeta() + "/Abonado.txt";
+        BufferedReader br = new BufferedReader(new FileReader(idFichero));
+        if (br.readLine() == null) {
+            System.out.println("EL PUTO ARCHIVO ABONADO ESTÁ VACÍO");
+            return null;
+        }
 
         // Variables para guardar los datos que se van leyendo
         String[] tokens;
@@ -50,16 +57,13 @@ public class restaurarCopiaDeSeguridad {
             // Mientras haya líneas por leer
             while (datosFichero.hasNextLine()) {
                 linea = datosFichero.nextLine();
-              
-
-             
 
                 //convert String to LocalDate
                 System.out.println(linea);
                 tokens = linea.split("\\|");
-                  for(int i = 0; i < tokens.length ; i++){
-                      System.out.println(tokens[i]);
-                  } 
+                for (int i = 0; i < tokens.length; i++) {
+                    System.out.println(tokens[i]);
+                }
                 abonado.setPk(Integer.parseInt(tokens[0]));
                 abonado.setNombre(tokens[1]);
                 abonado.setTipoDeAbono(tokens[2]);
@@ -84,16 +88,16 @@ public class restaurarCopiaDeSeguridad {
     public static String hallarUltimaCarpeta() {
         File f = new File("./backup");
         File[] ficheros = f.listFiles();
-      
 
         return ficheros[ficheros.length - 1].getName();
     }
-     public static ArrayList<VehiculoVO> restaurarTablaVehiculos() {
+
+    public static ArrayList<VehiculoVO> restaurarTablaVehiculos() {
         ArrayList<VehiculoVO> vehiculos = new ArrayList<>();
         VehiculoVO vehiculo = new VehiculoVO();
-        
+
         // Fichero a leer
-        String idFichero = "./backup/"+restaurarCopiaDeSeguridad.hallarUltimaCarpeta()+"/Vehiculo.txt";
+        String idFichero = "./backup/" + restaurarCopiaDeSeguridad.hallarUltimaCarpeta() + "/Vehiculo.txt";
 
         // Variables para guardar los datos que se van leyendo
         String[] tokens;
@@ -109,16 +113,15 @@ public class restaurarCopiaDeSeguridad {
             // Mientras haya líneas por leer
             while (datosFichero.hasNextLine()) {
                 linea = datosFichero.nextLine();
-       
+
                 System.out.println(linea);
                 tokens = linea.split("\\|");
-                 
+
                 int x = Integer.parseInt(tokens[2]);
-               
-                
-                 vehiculo.setMatricula(tokens[0]);
-                 vehiculo.setTipoVehiculo(tokens[1]);
-                 vehiculo.setCodPlaza(x);
+
+                vehiculo.setMatricula(tokens[0]);
+                vehiculo.setTipoVehiculo(tokens[1]);
+                vehiculo.setCodPlaza(x);
 
             }
             vehiculos.add(vehiculo);
@@ -128,59 +131,54 @@ public class restaurarCopiaDeSeguridad {
         }
         return vehiculos;
     }
-     
-     public static ArrayList<PlazaVO> restaurarTablaPlazas() {
+
+    public static ArrayList<PlazaVO> restaurarTablaPlazas() {
         ArrayList<PlazaVO> plazas = new ArrayList<>();
-        PlazaVO plaza = new PlazaVO();
-        
-        // Fichero a leer
-        String idFichero = "./backup/"+restaurarCopiaDeSeguridad.hallarUltimaCarpeta()+"/Plaza.txt";
 
-        // Variables para guardar los datos que se van leyendo
+        // Archivo a leer
+        String idArchivo = "./backup/" + restaurarCopiaDeSeguridad.hallarUltimaCarpeta() + "/Plaza.txt";
+
+        // Variables
         String[] tokens;
-        String linea = "";
+        String line = "";
 
-        System.out.println("Leyendo el fichero: " + idFichero);
+        System.out.println("Leyendo el archivo " + idArchivo);
 
         // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
         // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
         // las operaciones con el archivo
-        try (Scanner datosFichero = new Scanner(new FileReader(idFichero))) {
-
-            // Mientras haya líneas por leer
-            while (datosFichero.hasNextLine()) {
-                linea = datosFichero.nextLine();
-             
-
-             
-
-                //convert String to LocalDate
-                System.out.println(linea);
-                tokens = linea.split("\\|");
-          
-                
-                   plaza.setCodPlaza(Integer.parseInt(tokens[0]));
-                   plaza.setOcupado(Boolean.parseBoolean(tokens[1]));
-                   plaza.setReservado(Boolean.parseBoolean(tokens[2]));
-                   plaza.setTipoPlaza(tokens[3]);
-                   
-                
-                
-
+        try (Scanner datosFichero = new Scanner(new FileReader(idArchivo))) {
+            for (int x = 0; x < 45; x++) {
+                PlazaVO plaza = new PlazaVO();
+                System.out.println(x);
+                line = datosFichero.nextLine();
+                System.out.println(line);
+                tokens = line.split("\\|");
+                plaza.setCodPlaza(Integer.parseInt(tokens[0]));
+                plaza.setOcupado(Boolean.parseBoolean(tokens[1]));
+                plaza.setReservado(Boolean.parseBoolean(tokens[2]));
+                plaza.setTipoPlaza(tokens[3]);
+                plazas.add(plaza);
+                System.out.println("PLAZAS INTRODUCIDAS: " + plaza.toString());
+                System.out.println("ELEMENTOS DENTRO DE PLAZAS: " + plazas);
             }
-            plazas.add(plaza);
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("Hay " + plazas.size() + " número de plazas.");
+        for (PlazaVO plazita : plazas) {
+            System.out.println("Plazas devueltas: " + plazita.toString());
+        }
         return plazas;
     }
-     public static ArrayList<TicketVO> restaurarTablaTickets() {
+
+    public static ArrayList<TicketVO> restaurarTablaTickets() {
         ArrayList<TicketVO> tickets = new ArrayList<>();
         TicketVO ticket = new TicketVO();
-        
+
         // Fichero a leer
-        String idFichero = "./backup/"+restaurarCopiaDeSeguridad.hallarUltimaCarpeta()+"/Ticket.txt";
+        String idFichero = "./backup/" + restaurarCopiaDeSeguridad.hallarUltimaCarpeta() + "/Ticket.txt";
 
         // Variables para guardar los datos que se van leyendo
         String[] tokens;
@@ -196,25 +194,20 @@ public class restaurarCopiaDeSeguridad {
             // Mientras haya líneas por leer
             while (datosFichero.hasNextLine()) {
                 linea = datosFichero.nextLine();
-                
-
-             
 
                 //convert String to LocalDate
                 System.out.println(linea);
                 tokens = linea.split("\\|");
-                 
-                
-                 ticket.setCodTicket(Integer.parseInt(tokens[0]));
-                 ticket.setCodPlaza(Integer.parseInt(tokens[1]));
-                 ticket.setMatricula(tokens[2]);
-                 ticket.setFechaEntrada(LocalDate.parse(tokens[3]));
-                 ticket.setImporte(Double.parseDouble(tokens[4]));
-                 ticket.setPin(tokens[5]);
-                 ticket.setHoraEntrada(LocalTime.parse(tokens[6]));
-                 ticket.setHoraSalida(LocalTime.parse(tokens[7]));
-                 ticket.setFechaSalida(LocalDate.parse(tokens[8]));
-          
+
+                ticket.setCodTicket(Integer.parseInt(tokens[0]));
+                ticket.setCodPlaza(Integer.parseInt(tokens[1]));
+                ticket.setMatricula(tokens[2]);
+                ticket.setFechaEntrada(LocalDate.parse(tokens[3]));
+                ticket.setImporte(Double.parseDouble(tokens[4]));
+                ticket.setPin(tokens[5]);
+                ticket.setHoraEntrada(LocalTime.parse(tokens[6]));
+                ticket.setHoraSalida(LocalTime.parse(tokens[7]));
+                ticket.setFechaSalida(LocalDate.parse(tokens[8]));
 
             }
             tickets.add(ticket);
@@ -225,24 +218,21 @@ public class restaurarCopiaDeSeguridad {
         return tickets;
     }
 
-     public static void restaurarBaseDeDatos(){
-         ArrayList<AbonadoVO> abonados = restaurarCopiaDeSeguridad.restaurarTablaAbonados();
-         ArrayList<PlazaVO> plazas =  restaurarCopiaDeSeguridad.restaurarTablaPlazas();
-         ArrayList<TicketVO> tickets = restaurarCopiaDeSeguridad.restaurarTablaTickets();
-         ArrayList<VehiculoVO> vehiculos = restaurarCopiaDeSeguridad.restaurarTablaVehiculos();
-         EnviarDatos.resetBaseDeDatos();
-         EnviarDatos.restaurarBaseDeDatos(tickets,plazas,abonados,  vehiculos);
-         
-     }
+    public static void restaurarBaseDeDatos() throws IOException {
+        ArrayList<AbonadoVO> abonados = restaurarCopiaDeSeguridad.restaurarTablaAbonados();
+        ArrayList<PlazaVO> plazas = restaurarCopiaDeSeguridad.restaurarTablaPlazas();
+        ArrayList<TicketVO> tickets = restaurarCopiaDeSeguridad.restaurarTablaTickets();
+        ArrayList<VehiculoVO> vehiculos = restaurarCopiaDeSeguridad.restaurarTablaVehiculos();
+        EnviarDatos.resetBaseDeDatos();
+        EnviarDatos.restaurarBaseDeDatos(tickets, plazas, abonados, vehiculos);
+
+    }
 
     public static void main(String[] args) {
 //        System.out.println(restaurarCopiaDeSeguridad.restaurarTablaAbonados());
-        
-        System.out.println(restaurarCopiaDeSeguridad.restaurarTablaVehiculos());
-       
-   
 
-        
+        System.out.println(restaurarCopiaDeSeguridad.restaurarTablaVehiculos());
+
     }
 
 }
